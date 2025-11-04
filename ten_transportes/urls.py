81b86e13b4fe01c9f31c5e3e23fa6fc144e4e8b9
camilosanchez <juancamilosanchez@unicomfacauca.edu.co>
2025-11-04 12:58:15 -0500

    cambio template cliente y empresa terminados tiene responsive completado

--- ten_transportes/urls.py
+++ ten_transportes/urls.py
@@ -46,7 +46,7 @@ urlpatterns = [
     ),
     path(
         'accounts/password_reset/done/',
-        auth_views.PasswordResetDoneView.as_view(template_name='core/password_reset_done.html'),
+        auth_views.PasswordResetDoneView.as_view(template_name='cores/password_reset_done.html'),
         name='password_reset_done'
     ),
     path(
